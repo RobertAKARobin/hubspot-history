627560ec43c92737085db7493e7e11f3714a0888
Robin Thomas <>
2017-10-17 16:12:48 -0500

    Gets form data on input

--- public/main.js
+++ public/main.js
@@ -29,15 +29,38 @@ Date.prototype.toArray = function(){
 	var date = this;
 	return [date.getFullYear(), date.getMonthWithZeroes(), date.getDateWithZeroes()];
 }
+Object.defineProperty(Object.prototype, 'merge', {
+	enumerable: false,
+	value: function(input){
+		var object = this;
+		for(var key in object){
+			input[key] = object[key];
+		}
+		return input;
+	}
+})
+m.input = function(stream){
+	return {
+		value: stream(),
+		oninput: function(event){
+			event.redraw = false;
+			stream(event.target.value);
+		}
+	}
+}
 
 var Controls = (function(){
 
-	var DealProperties = {
-		all: [],
-		byName: {}
-	}
+	var DealProperties = [];
 
 	var Today = (new Date()).toArray();
+
+	var Input = {
+		year: m.stream(Today[0]),
+		month: m.stream(Today[1]),
+		day: m.stream(Today[2]),
+		properties: []
+	}
 
 	return {
 		oninit: function(){
@@ -45,10 +68,9 @@ var Controls = (function(){
 				method: 'GET',
 				url: './deals/properties'
 			}).then(function(properties){
-				DealProperties.all = Object.values(properties).sortOn(function(item){
+				DealProperties = Object.values(properties).sortOn(function(item){
 					return (item.name || item.label);
 				});
-				DealProperties.byName = properties;
 			});
 		},
 		view: function(){
@@ -56,37 +78,50 @@ var Controls = (function(){
 				m('label', [
 					m('span', "You want to go back in time and see how Hubspot's Deals looked on which date?"),
 					m('div.numbers', [
-						m('input[type=number]', {
+						m('input[type=number]', m.input(Input.year).merge({
 							min: 2012,
 							max: 2022,
-							placeholder: 'YYYY',
-							value: Today[0]
-						}),
-						m('input[type=number]', {
+							placeholder: 'YYYY'
+						})),
+						m('input[type=number]', m.input(Input.month).merge({
 							min: 1,
 							max: 12,
-							placeholder: 'MM',
-							value: Today[1]
-						}),
-						m('input[type=number]', {
+							placeholder: 'MM'
+						})),
+						m('input[type=number]', m.input(Input.day).merge({
 							min: 1,
 							max: 31,
-							placeholder: 'DD',
-							value: Today[2]
-						})
+							placeholder: 'DD'
+						}))
 					])
 				]),
 				m('label', [
-					m('span', "Which fields do you want to download? Pick multiple by holding 'Shift.'"),
+					m('span', "Which properties do you want to download? Pick multiple by holding 'Shift' while you click."),
 					m('select', {
-						multiple: true
-					}, DealProperties.all.map(function(property){
+						multiple: true,
+						onchange: function(event){
+							event.redraw = false;
+							var select = event.target;
+							var options = select.options;
+							Input.properties = [];
+							for(var i = 0; i < options.length; i++){
+								if(options[i].selected){
+									Input.properties.push(options[i].value);
+								}
+							}
+						}
+					}, DealProperties.map(function(property){
 						return m('option', {
 							value: property.name
 						}, property.label || property.name)
 					}))
 				]),
-				m('button', 'Download to Excel-friendly Format')
+				m('button', {
+					onclick: function(event){
+						event.redraw = false;
+						console.log(JSON.stringify(Input))
+					}
+				}, 'Download to Excel-friendly Format')
 			]
 		}
 	}
