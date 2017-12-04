8188b55a2a8ba53038aee6368a4018177e0925e7
RobertAKARobin <robertgfthomas@gmail.com>
2017-12-03 19:36:27 -0600

    Changed location of fancy math operators

--- public/snapshot.js
+++ public/snapshot.js
@@ -56,13 +56,13 @@ Components.snapshot = function(){
 			.replace(/\bAND\b/gi, '&&')
 			.replace(/\bOR\b/gi, '||')
 			.replace(/\bNOT\s+/gi, '!')
-			.replace(/≠/g, '!=')
-			.replace(/≥/g, '>=')
-			.replace(/≤/g, '<=')
 			.replace(/\s+HAS\s+(%%%%)/gi, '.indexOf(%%%%) > -1')
 			.replace(/([^<>])(=+)/g, function(nil, modifier, equalses){
 				return modifier + (equalses.length == 1 ? '==' : equalses);
 			})
+			.replace(/≠/g, '!=')
+			.replace(/≥/g, '>=')
+			.replace(/≤/g, '<=')
 			.replace(/%%%%/g, function(){
 				return quotes.shift();
 			});
